91e30cb9144abd5fc24e2a1e7f188d04a29b6f6b
Victor <mvictorl@gmail.com>
2021-01-09 21:19:20 +0300

    3. useMemo

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
+
+function testSum(x) {
+  console.log('testSum. counter =', x)
+  return x + x
+}
 
 const Post = ({ title, body, name, email }) => {
+  const [ counter, setCounter ] = useState(0)
+  const [ isGreen, setIsGreen ] = useState(false)
+
+  const sum = useMemo(() => testSum(counter), [ counter ])
+
   return (
     <div className="article-content">
       <div className="article-title">
         <a href="/">{ title }</a>
         <a href="/">{ name }</a>
       </div>
-      <p className="article-text">
+      <p
+        className="article-text"
+        style={ { color: isGreen ? 'green' : 'red' } }
+        onClick={ () => setIsGreen(!isGreen) }
+      >
         { body }
         { email }
       </p>
+      { sum }&nbsp;
+      <button onClick={ () => setCounter(counter + 1) }>+</button>
     </div>
   )
 }
 
-export default Post
+export default React.memo(Post)
